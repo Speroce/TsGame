4d8c11118c773cf61e63892d493dc441de2319ee
Speroce <speroce@yandex.ru>
2021-06-30 02:17:09 +0700

    Всё по-новому

---
+++ app.ts
@@ -0,0 +1,4 @@
+let mess222age: string = 'Hello, World!';
+let ses: string = 'papa';
+const a: string = mess222age + ses;
+console.log(a);
--- index.ts
+++
@@ -1,2 +0,0 @@
-let mess222age: string = 'Hello, World!';
-console.log(mess222age);
